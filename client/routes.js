35907cab1f5b18dd2a42030d60d4208075ccb02a
Saul Hardman <hello@iamsaul.co.uk>
2015-01-07 00:48:56 +0000

    feat(Expenses): add totals to filtered expenses

--- client/routes.js
+++ client/routes.js
@@ -1,18 +1,6 @@
 Router.route('/', function () {
   var expenses = Expenses.all().fetch();
-
-  var tagCounts = expenses.reduce(function (tagCounts, value) {
-    value.tags.forEach(function (value) {
-      if (!tagCounts.hasOwnProperty(value)) {
-        tagCounts[value] = 1;
-      } else {
-        tagCounts[value] += 1;
-      }
-    });
-
-    return tagCounts;
-  }, {});
-
+  var tagCounts = Expenses.tagCounts();
   var totals = [
     {
       name: 'total',
@@ -44,7 +32,12 @@ Router.route('/', function () {
 
   console.log('Expenses: ', expenses, totals);
 
-  this.render('dashboard', { data: { expenses: expenses, totals: totals } });
+  this.render('dashboard', {
+    data: {
+      expenses: expenses,
+      totals: totals,
+    }
+  });
 });
 
 Router.route('/expenses/add', function () {
@@ -53,30 +46,54 @@ Router.route('/expenses/add', function () {
 
 Router.route('/expenses/(.*)', function () {
   var tags = this.params[0].split('/');
-  var filteredExpenses = Expenses.find({ tags: { $in: tags } });
-  var expenses = Expenses.find();
-  var tagCounts = {};
-  expenses.map(function (value) {
-    value.tags.map(function (value) {
-      if (!tagCounts.hasOwnProperty(value)) {
-        tagCounts[value] = 1;
-      } else {
-        tagCounts[value] += 1;
-      }
-    });
-  });
-  filteredExpenses = filteredExpenses.map(function (value) {
+  var selector = { tags: { $in: tags } };
+  var expenses = Expenses.all(selector).fetch();
+  var tagCounts = Expenses.tagCounts();
+  var totals = [
+    {
+      name: 'total',
+      amount: Expenses.total(selector)
+    },
+    {
+      name: 'today',
+      amount: Expenses.dailyTotal(selector)
+    },
+    {
+      name: 'weekly',
+      amount: Expenses.weeklyTotal(selector)
+    },
+    {
+      name: 'monthly',
+      amount: Expenses.monthlyTotal(selector)
+    },
+  ];
+
+  console.log('selector', selector, tags);
+
+  expenses.forEach(function (value) {
     value.tags = value.tags.map(function (value) {
       var selected = tags.indexOf(value) > -1;
       var href = selected ? '/expenses/' + tags.filter(function (tag) { return tag !== value; }).join('/') : '/expenses/' + tags.concat([value]).join('/');
+
       if (href === '/expenses/') {
         href = '/';
       }
-      console.log('Selected: ', value, selected, href);
-      return { name: value, count: tagCounts[value], selected: selected, href: href };
+
+      return {
+        name: value,
+        count: tagCounts[value],
+        selected: selected,
+        href: href,
+      };
     });
-    return value;
   });
-  console.log('Expenses:', filteredExpenses.length, filteredExpenses);
-  this.render('dashboard', { data: { expenses: filteredExpenses, tag: this.params.tag } });
+
+  console.log('Expenses:', expenses.length, expenses);
+
+  this.render('dashboard', {
+    data: {
+      expenses: expenses,
+      totals: totals,
+    }
+  });
 });
