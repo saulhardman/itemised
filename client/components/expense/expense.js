adf86984f7ea2365142539608a936f53cd8979ca
Saul Hardman <hello@iamsaul.co.uk>
2015-01-15 23:17:41 +0000

    refactor(Expenses): refactor into Expense class
    - Refine drag to delete logic and fix some bugs
    - Switch from Velocity to CSS animations
    - Use Meteor method rather than inserting documents directly

--- client/components/expense/expense.js
+++ client/components/expense/expense.js
@@ -1,166 +1,191 @@
-Template.expense.rendered = function () {
-  this.fastClick = FastClick.attach(this.$('.js-expense')[0]);
-};
-
-Template.expense.destroyed = function () {
-  this.fastClick.destroy();
-};
-
-Template.expense.helpers({
-  hasTags: function () {
-    return this.tags.length !== 0;
-  }
-});
-
-Template.expense.events({
-  'touchstart .js-tags, touchmove .js-tags, touchend .js-tags': function (event) {
-    event.stopPropagation();
-  },
-  'touchstart .js-expense': function (event) {
-    var touch;
-
-    if (SlideToDelete.active) {
-      return false;
-    }
-
-    SlideToDelete.active = true;
-
-    touch = event.originalEvent.touches[0];
-
-    this.slideToDelete = new SlideToDelete(event.currentTarget, {
-      startedAt: touch.clientX,
-      scrollPosition: touch.clientY,
-    });
-  },
-  'touchmove .js-expense': function (event) {
-    if (!this.slideToDelete.isDestroyed) {
-      this.slideToDelete.move(event);
-    }
-  },
-  'touchend .js-expense': function (event) {
-    if (!this.slideToDelete.isDestroyed) {
-      this.slideToDelete.close(this._id);
-    }
-  }
-});
-
-var SlideToDelete = function (element, options) {
-  this.element = element;
-  this.$element = $(element);
-
-  this.startedAt = options.startedAt;
-  this.scrollPosition = options.scrollPosition;
-
-  this.$container = this.$element.find('.js-container');
-  this.$content = this.$element.find('.js-content');
+var Expense = function (template) {
+  this.template = template;
 
   return this;
 };
 
-SlideToDelete.active = false;
+Expense.isActive = false;
+Expense.isScrolling = false;
 
-SlideToDelete.prototype = {
+Expense.prototype = {
   moveThreshold: 48,
   deleteThreshold: 96,
   directions: { true: 'right', false: 'left' },
   scrollingThreshold: 12,
-  isDestroyed: false,
-  move: function move(event) {
+  init: function init() {
+    this.$element = this.template.$('.js-expense');
+    this.$container = this.$element.find('.js-container');
+    this.$content = this.$element.find('.js-content');
+
+    this.$container.css('max-height', this.$container.height());
+
+    this.isMoving = false;
+    this.isClosing = false;
+
+    this.fastClick = FastClick.attach(this.$element[0]);
+  
+    return this;
+  },
+  onTouchStart: function onTouchStart(event) {
+    var touch;
+
+    if (Expense.isActive || this.isClosing) {
+      return;
+    }
+
+    Expense.isScrolling = false;
+
+    Expense.isActive = true;
+
+    touch = event.originalEvent.touches[0];
+
+    this.startedAt = {
+      x: touch.clientX,
+      y: touch.clientY
+    };
+  
+    return this;
+  },
+  onTouchMove: function onTouchMove(event) {
+    if (Expense.isScrolling || this.isClosing) {
+      return;
+    }
+
     var touch = event.originalEvent.touches[0];
-    var distance = touch.clientX - this.startedAt;
+    var distance = touch.clientX - this.startedAt.x;
     var absDistance = Math.abs(distance);
-    var scroll = Math.abs(touch.clientY - this.scrollPosition);
-    var position;
-    var absPosition;
+    var scrollDistance = Math.abs(touch.clientY - this.startedAt.y);
 
     if (this.isMoving) {
+
       event.preventDefault();
 
-      position = (touch.clientX - this.movedFrom);
-
-      if ((this.direction === 'left' && position > 0) ||
-          (this.direction === 'right' && position < 0)) {
-        position = 0; 
-      }
-
-      if (this.isDeletable) {
-        position = this.calculateFriction(position);
-
-        this.$icon.css('transform', 'translateX(' + this.getIconPosition(position) + 'px)');
-      }
-
-      absPosition = Math.abs(position);
-
-      this.$content.css('transform', 'translateX(' + position + 'px)');
-
-      if (this.isDeletable && absPosition < this.deleteThreshold) {
-
-        this.undeletable();
-
-      } else if (!this.isDeletable && absPosition >= this.deleteThreshold) {
-
-        this.deletable();
-
-      }
+      this.move(touch);
 
     } else if (absDistance >= this.moveThreshold) {
 
       this.startMoving(touch.clientX, distance > 0);
 
-    } else if (scroll >= this.scrollingThreshold) {
+    } else if (scrollDistance >= this.scrollingThreshold) {
 
-      this.destroy();
+      this.scrollDetected();
 
     }
-
-    this.position = position;
-  
+    
     return this;
   },
-  close: function close(id) {
-    if (this.isDeletable &&
-        Math.abs(this.position) >= this.deleteThreshold) {
-      $.Velocity(this.$container, {
-        translateX: { 'left': '-100%', 'right': '100%' }[this.direction]
-      }, {
-        duration: 200,
-        queue: false,
-        complete: function () {
-          $.Velocity(this.$container, {
-            height: 0,
-          }, {
-            duration: 200,
-            complete: this.destroy.bind(this, id, true)
-          });
-        }.bind(this)
-      });
+  onTouchEnd: function onTouchEnd() {
+    if (Expense.isScrolling) {
+      this.allowMoving();
+
+      return;
+    }
+
+    if (!this.isMoving || this.isClosing) {
+      return;
+    }
+
+    this.isClosing = true;
+
+    if (this.isDeletable) {
+
+      this.$container.addClass('expense__container--is-exiting-' + this.direction).css('max-height', 0).one(utils.transitionEnd, function (event) {
+        if (event.originalEvent.propertyName === 'max-height') {
+          this.remove();
+        }
+      }.bind(this));
+
     } else {
-      $.Velocity(this.$content, {
-        translateX: [0, this.position]
-      }, {
-        duration: 200,
-        complete: this.destroy.bind(this, id, false)
-      });
+
+      this.$content.addClass('expense__content--is-reset').css('transform', 'translateX(0)').one(utils.transitionEnd, function () {
+        this.$content.removeClass('expense__content--is-reset');
+
+        this.reset();
+      }.bind(this));
+
     }
   
     return this;
   },
-  destroy: function destroy(id, remove) {
-    if (remove) {
-      Session.set('deleted', (Session.get('deleted') || []).concat([id]));
+  move: function move(touch) {
+    var position = touch.clientX - this.movedFrom;
+    var absPosition;
 
-      Meteor.call('expenseDelete', id);
+    if ((this.direction === 'left' && position > 0) ||
+        (this.direction === 'right' && position < 0)) {
+
+      position = 0;
+
     }
 
+    if (this.isDeletable) {
+
+      position = this.calculateFriction(position);
+
+      this.$icon.css('transform', 'translateX(' + this.getIconPosition(position) + 'px)');
+
+    }
+
+    absPosition = Math.abs(position);
+
+    this.$content.css('transform', 'translateX(' + position + 'px)');
+
+    if (this.isDeletable && absPosition < this.deleteThreshold) {
+
+      this.undeletable();
+
+    } else if (!this.isDeletable && absPosition >= this.deleteThreshold) {
+
+      this.deletable();
+
+    }
+  
+    return this;
+  },
+  scrollDetected: function scrollDetected() {
+    Expense.isScrolling = true;
+  
+    return this;
+  },
+  allowMoving: function allowMoving() {
+    Expense.isScrolling = false;
+
+    Expense.isActive = false;
+  
+    return this;
+  },
+  reset: function reset() {
+    Expense.isActive = false;
+
+    this.isMoving = false;
+    this.isDeletable = false;
+    this.isClosing = false;
+
+    delete this.direction;
+    delete this.$icon;
+  
+    return this;
+  },
+  remove: function remove() {
+    var id = this.template.data._id;
+
+    Session.set('deleted', (Session.get('deleted') || []).concat([id]));
+
+    Meteor.call('expenseDelete', id);
+
+    Expense.isActive = false;
+
+    this.destroy();
+  },
+  destroy: function destroy() {
+    this.fastClick.destroy();
+
+    delete this.movedFrom;
     delete this.element;
     delete this.$element;
     delete this.$content;
     delete this.$icon;
 
-    SlideToDelete.active = false;
-
-    this.isDestroyed = true;
-  
     return this;
   },
   startMoving: function startMoving(position, direction) {
@@ -194,7 +219,7 @@ SlideToDelete.prototype = {
 
     classes.splice(classes.indexOf('expense__delete-icon--is-active'), 1);
 
-    this.$icon.attr('class', classes.join(' '));
+    this.$icon.attr('class', classes.join(' ')).css('transform', 'translateX(0)');
   
     return this;
   },
@@ -211,3 +236,45 @@ SlideToDelete.prototype = {
     return (absPosition - this.deleteThreshold) * sign;
   },
 };
+
+Template.expense.created = function () {
+  this.expense = new Expense(this);
+};
+
+Template.expense.rendered = function () {
+  this.expense.init();
+};
+
+Template.expense.destroyed = function () {
+  this.expense.destroy();
+
+  delete this.expense;
+};
+
+Template.expense.helpers({
+  hasTags: function () {
+    return this.tags.length !== 0;
+  }
+});
+
+Template.expense.events({
+  // 'click .js-tag-link': function (event) {
+  //   var $tagLink = $(event.currentTarget);
+  //   var $tagCount = $tagLink.find('.expense__tag-count');
+
+  //   $tagLink.toggleClass('expense__tag-link--is-selected');
+  //   $tagCount.toggleClass('expense__tag-count--is-selected');
+  // },
+  'touchstart .js-tags, touchmove .js-tags, touchend .js-tags': function (event) {
+    event.stopPropagation();
+  },
+  'touchstart .js-expense': function (event, template) {
+    template.expense.onTouchStart(event);
+  },
+  'touchmove .js-expense': function (event, template) {
+    template.expense.onTouchMove(event);
+  },
+  'touchend .js-expense': function (event, template) {
+    template.expense.onTouchEnd(event);
+  }
+});
