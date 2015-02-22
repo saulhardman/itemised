3ed8a92d19f17ff89c53709fc4fe07408b0f1b62
Saul Hardman <hello@iamsaul.co.uk>
2015-02-22 14:16:03 +0000

    feat(Fixtures): use moment to construct date

--- server/fixtures.js
+++ server/fixtures.js
@@ -9,7 +9,7 @@ if (count < minimum) {
       note: _.sample(['Cheese', 'Sushi', 'Sandwich', 'iPhone Case', 'Inner Tube']),
       amount: Math.round(Math.random() * 1000),
       location: _.sample(['Tesco', 'Pret a Manger', 'Stokey Bear\'s', 'Waitrose', 'Yo! Sushi', '']),
-      date: new Date(2015, 0, Math.ceil(Math.random() * new Date().getDate()), Math.floor(Math.random() * 24)),
+      date: moment().subtract(Math.round(Math.random() * 365), 'days').hour(_.sample(_.keys(utils.getTimes()))).toDate(),
       userId: '4kRGPMn3vGByF6Rmh',
       tagNames: _.sample(['food', 'essentials', 'drinks', 'leisure', 'activity'], Math.ceil(Math.random() * 5)),
       isDeleted: false,
