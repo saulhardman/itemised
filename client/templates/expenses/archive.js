2e764962cb1fda99cc385b2c6ea5283db110ab7e
Saul Hardman <hello@iamsaul.co.uk>
2015-03-01 09:46:52 +0000

    feat(Expenses): add headers on index and archive

--- client/templates/expenses/archive.js
+++ client/templates/expenses/archive.js
@@ -1,3 +1,15 @@
+Template.expenseArchive.helpers({
+  count: function () {
+    var filteredTagIds;
+
+    if ((filteredTagIds = Session.get('filteredTagIds') || []).length > 0) {
+      return Expenses.find({ isDeleted: true, tagIds: { $in: filteredTagIds } }).count();
+    }
+
+    return Expenses.find({ isDeleted: true }).count();
+  },
+});
+
 Template.expenseArchive.rendered = function () {
   this.find('#js-expenses')._uihooks = expensesUiHooks;
 };
